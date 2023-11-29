f34c3bfc8dbc61707613fbfa190d54da49e126dd
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-11-29 02:41:15 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,7 +5,7 @@
     The two digits must be different - 01 and 10 are considered identical.
     """
 for digit_one in range(0, 10):
-    for digit_two in range(digit1 + 1, 10):
+    for digit_two in range(digit_one + 1, 10):
         if digit_one == 8 and digit_two == 9:
             print("{}{}".format(digit_one, digit_two))
         else:
