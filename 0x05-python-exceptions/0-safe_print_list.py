8c9b43c523669271bd0ced8a1d6c830e9db706da
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-12-18 03:29:32 -0800

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-safe_print_list.py
+
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (count)
