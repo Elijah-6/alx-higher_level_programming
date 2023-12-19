980f642075a41084b2b7ff6cc88ad771421d99a6
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-12-19 02:56:09 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Square:
+    """ A class that defines a square by: (based on 0-square.py)"""
+
+    def __init__(self, size = 0):
+        """ constructor for Square class with private attribute (size)
+        __size : the size of the square. """
+        self.__size = size
