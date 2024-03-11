91c82447facd0c98f691778a7440c72de900450e
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-03-11 22:52:12 +0000

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -9,10 +9,6 @@ function factorial (num) {
   } else if (num === 0) {
     return 1;
   } else {
-    let result = 1;
-    for (let i = 1; i <= num; i++) {
-      result *= i;
-    }
-    return result;
+    return num * factorial(num - 1);
   }
 }
