511de98999f0ed8c184126ec16289bf3eea00c3e
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-03-12 01:05:08 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-const numbers = argv.slice(2);
-// numbers.forEach(number => parseInt(number))
-numbers.sort((a, b) => b - a);
-console.log(numbers[1]);
+const numbers = argv.slice(2).map(Number);
+if (numbers.length <= 1) {
+  console.log(0);
+} else {
+  const uniqueNumbers = [...new Set(numbers)]; // Remove duplicates
+  const sortedNumbers = uniqueNumbers.sort((a, b) => b - a); // Sort in descending order
+  console.log(sortedNumbers[1]);
+}
