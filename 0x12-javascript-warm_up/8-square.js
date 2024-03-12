8e18e53421a6e7781328beb5dc590946be7cd9ba
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-03-12 01:14:18 +0000

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 const size = parseInt(argv[2]);
-for (let i = 0; i < size; i++) {
-  let line = '';
-  for (let j = 0; j < size; j++) {
-    line += 'X';
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let line = '';
+    for (let j = 0; j < size; j++) {
+      line += 'X';
+    }
+    console.log(line);
   }
-  console.log(line);
 }
