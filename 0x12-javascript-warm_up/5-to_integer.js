f794d5e06dcca09b1b94cf49312d8b6019bb260e
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-03-12 01:24:00 +0000

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 const number = parseInt(argv[2]);
-!isNaN(number) ? console.log(`My number : ${number}`) : console.log('Not a number');
+// !isNaN(number) ? console.log(`My number : ${number}`) : console.log('Not a number');
+
+if (!isNaN(number)) {
+  console.log(`My number : ${number}`);
+} else {
+  console.log('Not a number');
+}
