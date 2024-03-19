c2dfe1f72d3c79a758656d559403b30894cb51fb
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-03-19 07:48:25 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """ Get MySQL credentials and state name from command line arguments"""
+    username, password, db_name, search_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name, charset="utf8")
+    cursor = db.cursor()
+
+    # Execute query to retrieve states matching the provided name
+    sql_query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
+    cursor.execute(sql_query, (search_name,))
+    rows = cursor.fetchall()
+
+    # Display results
+    for row in rows:
+        print(row)
+
+    # Close the database connection
+    cursor.close()
+    db.close()
+
