ce06befb7a2a1e03b27117e15c61605b3c413cb3
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-03-19 07:34:34 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,23 @@
+#!/usr/bin/python3
+
 import MySQLdb
+"""MySQLdb for executing sql commands"""
+
 import sys
 
 if __name__ == "__main__":
-    # Get MySQL credentials from command line arguments
+    """ Get MySQL credentials from command line arguments"""
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Connect to MySQL server
+    """ Connect to MySQL server"""
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name, charset="utf8")
     cursor = db.cursor()
 
-    # Execute query to retrieve states
+    """ Execute query to retrieve states """
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
 
-    # Display results
+    """ Display results"""
     for row in rows:
         print(row)
 
