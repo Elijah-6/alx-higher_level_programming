3254f56a3f692090b5b675e9e81cbe1078dab736
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-07-30 16:56:23 +0000

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const req = require('request');
 const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/${id}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 req.get(url, function (error, res, body) {
   if (error) {
     console.log(error);
