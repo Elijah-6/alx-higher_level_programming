7056e915da5cecf991c7c3e9b38cee451d560d7e
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-07-30 16:14:01 +0000

    all

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+request(url, function (error,response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
