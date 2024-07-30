2a2071adcded9e270827089b6de5715ef56bf1b4
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-07-30 16:43:05 +0000

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,4 +2,7 @@
 
 const fs = require('fs');
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]),{
+    flags: 'a',
+    encoding: 'utf8'
+});
